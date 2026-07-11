ba2df1c2e60573e22c1ac9f89c358358af4c283e
Savan kajo <95406476+savankajo@users.noreply.github.com>
2026-07-10 18:00:49 -0700

    Use configured database URL in production

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,7 @@
 import { PrismaClient } from '@prisma/client'
-import { getConnectionString } from '@netlify/database'
-
-function getDatabaseUrl() {
-    if (process.env.NETLIFY_DB_URL) {
-        return getConnectionString()
-    }
-
-    return process.env.DATABASE_URL
-}
 
 const prismaClientSingleton = () => {
-    const url = getDatabaseUrl()
+    const url = process.env.DATABASE_URL
 
     return new PrismaClient({
         datasources: url
